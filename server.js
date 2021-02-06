570fc1984be27f16fa01a0a40b84324ace0a5752
Benjamin Shostak <benshostak@rocketmail.com>
2021-02-06 06:51:36 -0800

    made index.handlebars

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const { text } = require("express");
 const express = require("express");
 const express = require("express");
 const app = express();
+const expHBars = require("express-handlebars");
 
 
+app.engine('handlebars', expHBars());
+app.set('view engine', 'handlebars');
 
 app.listen(8080, () => {
     console.log('Server is starting at port ', 8080);
