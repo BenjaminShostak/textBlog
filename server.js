bfdd3e841b0b89360333ea84b7614ceacbaa7ae1
Benjamin Shostak <benshostak@rocketmail.com>
2021-02-11 04:01:09 -0800

    updated {title} for each page

--- server.js
+++ server.js
@@ -3,16 +3,22 @@ const app = express();
 const expHBars = require("express-handlebars");
 
 
-app.engine('handlebars', expHBars());
+app.engine('handlebars', expHBars({
+    defaultLayout: 'main',
+layoutsDir: path.join(
+__dirname, 'views/mainLayout')}));
+
 app.set('view engine', 'handlebars');
+
+
 
 //Routing
 app.get('/', ( req , res) => {
-    res.render('index');
+    res.render('index', {title: 'homepage'});
 });
 
 app.get('/signIn', ( req, res) => {
-    res.render('signIn');
+    res.render('signIn', {title: 'signInPage'});
 });
 app.listen(8080, err =>  {
     if (err) {
