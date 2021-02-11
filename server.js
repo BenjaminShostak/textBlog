dc53a377204bb865c0f68e4d3507adea1fe7a160
Benjamin Shostak <benshostak@rocketmail.com>
2021-02-11 04:29:03 -0800

    updated {{name}} and {{age}}  for homepage

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const expHBars = require("express-handlebars");
+const path = require("path");
 
 
 app.engine('handlebars', expHBars({
     defaultLayout: 'main',
-layoutsDir: path.join(
-__dirname, 'views/mainLayout')}));
+    layoutsDir: path.join(__dirname, 'views/mainLayout')}));
 
 app.set('view engine', 'handlebars');
 
@@ -14,7 +14,10 @@ app.set('view engine', 'handlebars');
 
 //Routing
 app.get('/', ( req , res) => {
-    res.render('index', {title: 'homepage'});
+    res.render('index', {title: 'homepage',
+    name: 'Benjamin Shostak',
+    age: '33'
+    });
 });
 
 app.get('/signIn', ( req, res) => {
